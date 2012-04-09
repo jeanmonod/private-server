07fb1f78c4aad546cb37a304e41a77afa806f90b
Gilles Cornu <git@gilles.cornu.name>
2012-04-10 01:39:34 +0200

    Fixes for php_stack cookbook
    - use apache_site built-in feature to disable default site
    - create /var/www/hello-world only if it does not exists yet
    - no need to explicitly reload/restart apache2 service, because web_app
    and apache_site definitions request for it (with only one service
    operation at the end of the recipe)

--- cookbooks/php_stack/recipes/default.rb
+++ cookbooks/php_stack/recipes/default.rb
@@ -2,25 +2,27 @@ require_recipe "apache2"
 require_recipe "apache2::mod_php5"
 require_recipe "php"
 
-# Remove the default apache site
-execute "disable-default-site" do
-  command "sudo a2dissite default"
-  notifies :restart, resources(:service => "apache2")
+# Disable the default apache site
+apache_site 'default' do
+  enable false
 end
 
-# Create a basic PHP website in /tmp
+# Create a basic PHP website 
 directory "/var/www/hello-world" do
   mode 0777
   action :create
+  not_if do 
+    File.directory? "/var/www/hello-world"
+  end
 end
 template "/var/www/hello-world/index.php" do
   source "hello-world.php"
   mode 0666
 end
 
-# Configure the apache vhost
+# Configure and enable basic apache vhost
 web_app "hello-world" do
   template "hello-world.conf.erb"
   docroot "/var/www/hello-world"
-  notifies :restart, resources(:services => "apache2")
 end
+
